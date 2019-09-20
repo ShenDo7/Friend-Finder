fd2dfcbab4df96e8ef5819017b3ea217d4fbf363
shendo7 <rodneyrjames@gmail.com>
2019-09-20 10:21:01 -0700

    logic adjusted

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,5 @@
 const friends = require("./FriendFinder/app/data/friends");
-const userScores = require("./survey.html");
+// const userScores = require("./survey.html");
 
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
@@ -8,21 +8,26 @@ module.exports = function(app) {
     });
   });
 
-  app.post("/api/friends", userScores, function(req, res) {
+  app.post("/api/friends", function(req, res) {
+    const userScore = req.body.scores;
+    console.log("test" + req.body.scores);
     var match;
     var difference = 100;
     friends.forEach(friend => {
       var temp = 0;
       for (i = 0; i < friend.scores.length; i++) {
         temp += Math.abs(parseInt(friend.scores[i] - userScore[i]));
+        // console.log(temp);
       }
       if (temp < difference) {
+        // console.log("" + difference);
         difference = temp;
         match = friend;
+        // console.log(friend);
       }
     });
-    console.log(req.body.data);
-    friends.push(req.body.data);
+    friends.push(req.body);
+    console.log(match);
     res.send(match);
   });
 };
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8080;
 
 // local files
-var friends = require ("./FriendFinder/app/data/friends")
+var friends = require("./FriendFinder/app/data/friends");
 
 const app = express();
 
@@ -15,19 +15,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 
 // Routes
-require("./apiRoutes")(app)
-require("./htmlRoutes")(app)
-
+require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
 
 module.exports = app;
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./home.html"));
-   });
-
+  res.sendFile(path.join(__dirname, "./home.html"));
+});
 
 // Start Server
-app.listen(PORT, ()=>{
-    console.log(`Server listing on port ${PORT}!`)
-})
-
+app.listen(PORT, () => {
+  console.log(`Server listing on port ${PORT}!`);
+});
